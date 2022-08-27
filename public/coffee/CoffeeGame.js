ffa34811ddf394119099632a7dc7469ca686bf3e
vvdleun <22765050+vvdleun@users.noreply.github.com>
2022-08-28 00:28:56 +0200

    WIP

--- public/coffee/CoffeeGame.js
+++ public/coffee/CoffeeGame.js
@@ -1,4 +1,7 @@
 import { Game } from "../berrajs/core/Game";
+import { PrintBoldEvent } from "../berrajs/events/PrintBoldEvent";
+import { PrintEvent } from "../berrajs/events/PrintEvent";
+import { PrintLineEvent } from "../berrajs/events/PrintLineEvent";
 import { SetActiveRoomEvent } from "../berrajs/events/SetActiveRoomEvent";
 import { LivingRoom } from "./rooms/LivingRoom";
 import { OutsideHouse } from "./rooms/OutsideHouse";
@@ -15,6 +18,20 @@ export class CoffeeGame extends Game {
             rooms.push(new OutsideHouse(state));
 
             initialTurn.addEvent(new SetActiveRoomEvent(null, START_ROOM_ID));
+            initialTurn.addEvent(new PrintEvent("Welcome to the "));
+            initialTurn.addEvent(new PrintBoldEvent("Coffee"));
+            initialTurn.addEvent(new PrintLineEvent(" game!"));
+            initialTurn.addEvent(new PrintLineEvent("This is a small game to demonstrate and develop/debug the Berra.js adventure game engine."));
+            initialTurn.addEvent(new PrintEvent("It was heavily inspired by a small Dutch graphical adventure game, called \""));
+            initialTurn.addEvent(new PrintBoldEvent("Koffie"));
+            initialTurn.addEvent(new PrintLineEvent("\", which featured a text parser and was written in Turbo Pascal for DOS and published in 1988. First a public domain CGA "
+                    + "version was released, and later that year the sequel and a more impressive EGA version was released, now as shareware. Both versions came with the source code. "
+                    + "As a Sierra On-Line fan and budding Turbo Pascal programmer, back in the day, I was rather impressed."));
+            initialTurn.addEvent(new PrintEvent("This game is tributed to the original authors: programmer "));
+            initialTurn.addEvent(new PrintBoldEvent("J.A.M. Kleijn"));
+            initialTurn.addEvent(new PrintEvent(" (living in Rotterdam at that time) and graphical artist "));
+            initialTurn.addEvent(new PrintBoldEvent("W. van Heerebeek"));
+            initialTurn.addEvent(new PrintLineEvent(" (for the EGA version)."));
         });
     }
  }
